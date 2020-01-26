81989ea9b4a2dedab9fc3096708abe33b57b5bf4
“Andrew <abiv23@gmail.com>
2020-01-26 14:46:47 -0700

    hero image reworked as background image

--- portfolio-site/gatsby-browser.js
+++ portfolio-site/gatsby-browser.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+ // global reset
 import "./src/styles/global-reset.scss"
+// fonts
+import "./src/styles/fonts.scss"
+
+// ie polyfill for gastsby background image
+export const onClientEntry = () => {
+    // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+    if (!(`IntersectionObserver` in window)) {
+      import(`intersection-observer`)
+      console.log(`# IntersectionObserver is polyfilled!`)
+    }
+  }
--- portfolio-site/src/pages/index.js
+++ portfolio-site/src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import { useStaticQuery, graphql} from "gatsby"
 import Img from "gatsby-image"
+import BackgroundImage from 'gatsby-background-image'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -25,34 +26,61 @@ const IndexPage = (props) => {
     }
   `)
 
-  function handleSizeChange() {
-    console.log(document.getElementsByTagName("img")[0].height);
-    return setHeroImgSize(document.getElementsByTagName("img")[0].height)
-  }
+  // function handleSizeChange() {
+  //   console.log(document.getElementsByTagName("img")[0].height);
+  //   return setHeroImgSize(document.getElementsByTagName("img")[0].height)
+  // }
 
-  useEffect(() => {
-    window.addEventListener("resize", handleSizeChange())
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleSizeChange())
     
-    return () => {
-      window.removeEventListener("resize", handleSizeChange())
-    }
-  },[]);
+  //   return () => {
+  //     window.removeEventListener("resize", handleSizeChange())
+  //   }
+  // },[]);
 
 
   return (
     <Layout >
       <SEO title="Andrew Biviano - Front End Developer &amp; Digital Marketing - Denver, CO" description="Web professional with strong background in customer acquisition and optimization. 14+ years of working with developers and developing solutions to the ecommerce conversion funnel."/>
-      <div className={styles.heroImageContainer} onMouseMove={(e) => e.clientY < '50' ? setTopNavVisible(true) : setTopNavVisible(false)}>
-        <Img fluid={data.file.childImageSharp.fluid} />
-        <div className={styles.heroGradientContainer} style={{top: topNavVisible ? '50px' : '0', height: heroImgSize}}>
-            <h1>Frontend Developer</h1>
-            <p>working hard to make websites awesome.</p>
+        <BackgroundImage
+          Tag="section"
+          className={styles.heroImageContainer}
+          fluid={data.file.childImageSharp.fluid}
+          backgroundColor={`#040e18`}
+        >
+          <div className={styles.heroGradientContainer}>
+            <h1>Front end developer.</h1>
+            <p>working hard to make websites awesome</p>
+          </div>        
+        </BackgroundImage>
+        <div>
+          <p>Bacon ipsum dolor amet meatloaf shankle pork loin ribeye. Pork belly sirloin meatball ground round, pancetta buffalo leberkas tail cow chuck ribeye turkey picanha shank kevin. Flank short ribs corned beef, sausage rump frankfurter t-bone jowl. Rump cow kevin pork loin venison sirloin ground round cupim pastrami ribeye buffalo landjaeger. Cupim short ribs tenderloin pig, beef ribs meatloaf kielbasa ham shoulder.
+
+            Picanha doner landjaeger, pastrami pork belly swine kielbasa brisket shoulder. Bresaola buffalo short loin spare ribs. Pork belly prosciutto capicola flank porchetta. Pig alcatra andouille, tail flank bresaola shank chicken meatball. Shoulder frankfurter beef ribs leberkas shankle, beef bacon kielbasa ball tip hamburger landjaeger.
+
+            Burgdoggen ham tongue, chuck tail pig frankfurter spare ribs beef ribs filet mignon. Pastrami tri-tip pig ribeye salami ball tip hamburger beef ham hock pork chop fatback turkey. Shankle boudin cow beef ribs, pastrami jerky frankfurter pig t-bone. Shoulder ground round tail pork chop. Pork loin beef andouille shoulder bresaola, corned beef sirloin pork jowl pastrami strip steak porchetta meatloaf turkey. Kielbasa pork belly sausage, pancetta pork boudin tenderloin jerky cow pig corned beef drumstick t-bone.
+
+            Capicola brisket sausage alcatra short loin kevin prosciutto meatball biltong. Tail swine pork belly, cupim ribeye short loin short ribs burgdoggen jowl pancetta spare ribs. Kevin leberkas tail pastrami boudin pork buffalo bresaola flank cow shankle pork loin capicola ham hock kielbasa. Andouille swine chicken, strip steak beef ribs pork belly alcatra jerky pork loin drumstick porchetta. Picanha boudin tail, bresaola pork loin short loin short ribs corned beef. Hamburger brisket spare ribs, alcatra leberkas fatback ground round. Ground round shoulder filet mignon meatloaf ball tip.
+
+            Leberkas flank pancetta doner. Leberkas rump pancetta shankle short loin jerky. Tongue burgdoggen sausage, pork loin drumstick salami spare ribs frankfurter brisket ham hock andouille tenderloin fatback. Prosciutto turducken spare ribs corned beef, pork loin tail rump cow pork belly bresaola. Ham buffalo spare ribs, pork loin strip steak chislic jerky ham hock tongue chuck shank drumstick meatloaf short loin. Biltong frankfurter cupim buffalo, prosciutto kielbasa sausage.
+
+            Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!</p>
         </div>
-      </div>
-      
-      {/* <Link to="/page-2/">Go to page 2</Link> */}
+        
     </Layout>
   )
 }
+
+{/* onMouseMove={(e) => e.clientY < '50' ? setTopNavVisible(true) : setTopNavVisible(false)} */}
+        {/* <Img fluid={data.file.childImageSharp.fluid} />
+        <div className={styles.heroGradientContainer} style={{top: topNavVisible ? '50px' : '0', height: heroImgSize}}>
+        </div>
+        <div className={styles.heroText}>
+          <h1>Frontend Developer</h1>
+          <p>working hard to make websites awesome.</p>
+        </div> */}
+      
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
 
 export default IndexPage

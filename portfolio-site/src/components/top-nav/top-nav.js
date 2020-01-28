8bd5d734d18b3e485d9362a08e964f0a40207c7d
“Andrew <abiv23@gmail.com>
2020-01-28 14:54:04 -0700

    mobile top nav v1 complete

--- portfolio-site/src/components/top-nav/top-nav.js
+++ portfolio-site/src/components/top-nav/top-nav.js
@@ -1,9 +1,6 @@
-// import { Link } from "gatsby"
 import React, { useState, useEffect } from "react"
-// import { useStaticQuery, graphql} from "gatsby"
 
-import styles from "./top-nav.module.scss"
-// import Img from "gatsby-image"
+import styles from "./top-nav.module.scss";
 import useBreakpoints from '../breakpoint-manager/breakpoints';
 
 
@@ -15,6 +12,7 @@ const TopNav = (props) => {
 
   const [dropDownVisible, setDropDownVisible] = useState(false);
   const breakpoint = useBreakpoints();
+
   const Close = (props) => {
     return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
@@ -23,25 +21,36 @@ const TopNav = (props) => {
   };
   const Hamburger = (props) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+      </svg>
     )
   }
 
     return (
         <header>
           {breakpoint !== "desktop" &&
-            <nav style={{height: props.visible ? '50px' : '0'}} className={styles.navWrapper}>
-                <div className={styles.logo} onClick={(e) => setDropDownVisible() }>
-                  <img src={require("../../images/top-nav/white-hp-logo.png")} alt="andrew biviano website logo" />
+            <nav style={{height: props.visible ? '50px' : '0'}} className={styles.mobileNavWrapper}>
+                <div className={styles.mobileNavWrapper__logo} >
+                  <img style={{height: "50px", width: "60px"}} src={require("../../images/top-nav/white-hp-logo.png")} alt="andrew biviano website logo" />
                 </div>
-                <div style={{opacity: dropDownVisible ? "0" : "1"}} className={styles.hamburger}>
-                    <Hamburger />
+                <div className={styles.mobileNavWrapper__navIconContainer} onClick={() => setDropDownVisible(!dropDownVisible) }>
+                  <div style={{opacity: dropDownVisible ? "0" : "1", height: dropDownVisible ? "50" : "0"}} className={styles.navIconContainer__hamburger}>
+                      <Hamburger />
+                  </div>
+                  <div style={{opacity: dropDownVisible ? "1" : "0", height: dropDownVisible ? "0" : "50"}} className={styles.navIconContainer__close}>
+                      <Close />
+                  </div>
                 </div>
-                <div className={styles.close}>
-                    <Close />
+                <div className={styles.mobileNavWrapper__dropdown} style={{height: dropDownVisible ? "auto" : "-100vh", opacity: dropDownVisible ? "1" : "0"}}>
+                  <ul style={{marginLeft: dropDownVisible ? "0" : "-100vw", opacity: dropDownVisible ? "1" : "0"}}>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>LinkedIn</li>
+                    <li>Github</li>
+                  </ul>
                 </div>
             </nav>
           }
